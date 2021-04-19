f9289e8b6299a0a8b8452ccfbbd3e8c70a924c62
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2021-04-19 12:50:40 +0200

    Add existing file

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='style.css'>
+    
+    <script
+        src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+        crossorigin="anonymous">
+    </script>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+
+    <title>test</title>
+
+</head>
+<body>
+    <!-- exemple -->
+<input type="text" id="searchInput" placeholder="Search actor">
+<button id="submit">Submit</button>
+<div class="imageDiv">
+    <img src="Image/loading.gif">
+</div>
+<div id="message"></div>
+<ul id="pagination"></ul>
+
+
+
+<div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">×</button>
+                <h4 class="modal-title" id="modalTitleH4"></h4>
+            </div>
+            <div class="modal-body" id="modalBodyDiv">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+<script type="text/javascript" src="script.js"></script>
+</html>
---
+++ script.js
@@ -0,0 +1,96 @@
+$(document).ready(function () {
+    $("#submit").click(function (e) {
+        var validate = Validate();
+        $("#message").html(validate);
+        if (validate.length == 0) {
+            CallAPI(1);
+        }
+    });
+
+    function CallAPI(page) {
+        $.ajax({
+            url: "https://api.themoviedb.org/3/search/person?language=en-US&query=" + $("#searchInput").val() + "&page=" + page + "&include_adult=false",
+            data: { "api_key": "4694567a42a4950589090a37a9729f3f" },
+            dataType: "json",
+            success: function (result, status, xhr) {
+                var resultHtml = $("<div class=\"resultDiv\"><p>Names</p>");
+                for (i = 0; i < result["results"].length; i++) {
+
+                    var image = result["results"][i]["profile_path"] == null ? "Image/no-image.png" : "https://image.tmdb.org/t/p/w500/" + result["results"][i]["profile_path"];
+
+                    resultHtml.append("<div class=\"result\" resourceId=\"" + result["results"][i]["id"] + "\">" + "<img src=\"" + image + "\" />" + "<p><a>" + result["results"][i]["name"] + "</a></p></div>")
+                }
+
+                resultHtml.append("</div>");
+                $("#message").html(resultHtml);
+
+                Paging(result["total_pages"]);
+            },
+            error: function (xhr, status, error) {
+                $("#message").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+            }
+        });
+    }
+
+    function Validate() {
+        var errorMessage = "";
+        if ($("#searchInput").val() == "") {
+            errorMessage += "► Enter Search Text";
+        }
+        return errorMessage;
+    }
+
+    function Paging(totalPage) {
+        var obj = $("#pagination").twbsPagination({
+            totalPages: totalPage,
+            visiblePages: 5,
+            onPageClick: function (event, page) {
+                CallAPI(page);
+            }
+        });
+    }
+
+    $(document).ajaxStart(function () {
+        $(".imageDiv img").show();
+    });
+
+    $(document).ajaxStop(function () {
+        $(".imageDiv img").hide();
+    });
+});
+
+
+
+
+
+
+// modal 
+
+$("#message").on("click", ".result", function () {
+    var resourceId = $(this).attr("resourceId");
+    $.ajax({
+        url: "https://api.themoviedb.org/3/person/" + resourceId + "?language=en-US",
+        data: {
+            api_key: "3356865d41894a2fa9bfa84b2b5f59bb"
+        },
+        dataType: 'json',
+        success: function (result, status, xhr) {
+            $("#modalTitleH4").html(result["name"]);
+
+            var image = result["profile_path"] == null ? "Image/no-image.png" : "https://image.tmdb.org/t/p/w500/" + result["profile_path"];
+            var biography = result["biography"] == null ? "No information available" : result["biography"];
+
+            var resultHtml = "<p class=\"text-center\"><img src=\"" + image + "\"/></p><p>" + biography + "</p>";
+
+            $("#modalBodyDiv").html(resultHtml)
+
+            $("#myModal").modal("show");
+        },
+        error: function (xhr, status, error) {
+            $("#message").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+        }
+    });
+});
+
+
+
