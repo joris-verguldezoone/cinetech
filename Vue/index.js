8d4c1ca970d748bbdae3877a23086c396153d1ea
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-04-20 16:24:38 +0200

    sans this

--- Vue/index.js
+++ Vue/index.js
@@ -6,10 +6,9 @@ const vm = new Vue({
     },
     methods : {
         getMovies : function(){
-            const vm = this
             fetch('https://api.themoviedb.org/3/movie/'+this.movieId+'?api_key=8f560fa0d81bece7dce9718dd0d51a08')
                 .then(response=>response.json())
-                .then(json=>vm.content = json)
+                .then(json=>this.content = json)
         }
     },
     watch : {
