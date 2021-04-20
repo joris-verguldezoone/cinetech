1b2b08057b973f60d0c3ad3330ad1f4ff6ccf290
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-04-20 15:40:12 +0200

    View and api test

---
+++ Vue/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+
+</head>
+
+<body>
+    <div id="app">
+        <p>Indique un id pour avoir des détails sur un film:</p>
+        <input v-model="movieId">
+        <p>{{ content }}</p>
+    </div>
+    <script src="index.js" type="text/javascript"></script>
+</body>
+
+</html>
