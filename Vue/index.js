4d8a144dc5a3783d979dc62843e4f2e3955cc785
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2021-04-21 11:41:48 +0200

    néant absolue

--- Vue/index.js
+++ Vue/index.js
@@ -1,24 +1,24 @@
 const vm = new Vue({
-    el : '#app',
-    data : {
-        content : '',
-        movieId : ''
+    el: '#app',
+    data: {
+        content: '',
+        movieId: ''
     },
-    methods : {
-        getMovies : function(){
-            fetch('https://api.themoviedb.org/3/movie/'+this.movieId+'?api_key=8f560fa0d81bece7dce9718dd0d51a08')
-                .then(response=>response.json())
-                .then(json=>this.content = json)
+    methods: {
+        getMovies: function () {
+            fetch('https://api.themoviedb.org/3/movie/' + this.movieId + '?api_key=8f560fa0d81bece7dce9718dd0d51a08')
+                .then(response => response.json())
+                .then(json => this.content = json)
         }
     },
-    watch : {
-        movieId: function(newType,oldType){
+    watch: {
+        movieId: function (newType, oldType) {
             this.getMovies()
         }
     }
 })
 
-Vue.component('movie-item',{
+Vue.component('movie-item', {
     props: ['data'],
     template: '<div><h1>{{data.original_title}}</h1><p>{{data.overview}}</p><img v-bind:src="\'https://image.tmdb.org/t/p/w500//\'+ data.poster_path" ></div>'
 })
