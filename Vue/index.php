9ac2df5590516fc0d04834e1fc03210b098c5a4b
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-04-21 10:49:40 +0200

    First style for carrousel

--- Vue/index.php
+++ Vue/index.php
@@ -6,17 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
     <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-
+    <script src="index.js" type="text/javascript" async></script>
 </head>
 
 <body>
-    <div id="app">
-        <p>Indique un id pour avoir des détails sur un film:</p>
-        <input v-model="movieId">
-        <movie-item v-for="movie in results" v-bind:data="movie"></movie-item>
+    <div id="app" class="carrousel">
+        <movie-item class="carrousel__item" v-for="movie in results" v-bind:data="movie" :key="movie.id"></movie-item>
     </div>
-    <script src="index.js" type="text/javascript"></script>
 </body>
 
 </html>
