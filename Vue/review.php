21bf723bf39036f5893e31b4328c19fa84fa6bcc
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2021-04-23 10:33:04 +0200

    last commit

--- Vue/review.php
+++ Vue/review.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>mabite</title>
+    <title>review</title>
     <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
     <!-- <script src="https://unpkg.com/vue@next"></script> -->
 </head>
@@ -34,4 +34,4 @@ acces global aux valeur du content dans la div
 </body>
 
 </html>
-    <script src="script2.js" type="text/javascript"></script>
+    <script src="review.js" type="text/javascript"></script>
